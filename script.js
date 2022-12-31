a83e7f081ef94eabc803e66370ec12166587bea3
rishabhdhayal <76101214+rishabhdhayal@users.noreply.github.com>
2022-12-31 20:01:24 +0530

    upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// const phoneNav = document.getElementById('three-lines');
+
+// phoneNav.addEventListener('click',()=>{
+// })
+
+function phoneNav(){
+    document.getElementById('phone-nav').style.display = 'block';
+    event.preventDefault();
+};
+
